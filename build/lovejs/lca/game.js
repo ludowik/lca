38ac0b4eaa8ed8f1ee6bdcc9f8b6f10ef725015c
Ludowik <ludovic.milhau@gmail.com>
2022-05-05 22:57:36 +0200

    push

--- build/lovejs/lca/game.js
+++ build/lovejs/lca/game.js
@@ -284,6 +284,6 @@ Module.expectedDataFileDownloads++;
     }
 
   }
-  loadPackage({"package_uuid": "ef5cc484ba234c1c8b33da1117c8a310", "remote_package_size": 90410, "files": [{"filename": "/game.love", "crunched": 0, "start": 0, "end": 90410, "audio": false}]});
+  loadPackage({"package_uuid": "3d5b9ec57d0843988285e394e64a3976", "remote_package_size": 90410, "files": [{"filename": "/game.love", "crunched": 0, "start": 0, "end": 90410, "audio": false}]});
 
 })();
